c9d7128a1ccd3913547b2b372f7ef011a85cf4e0
Sourabh Suneja <64066437+sunejasourabh121@users.noreply.github.com>
2024-08-09 10:14:22 +0530

    Add files via upload

---
+++ exp5C.cpp
@@ -0,0 +1,49 @@
+
+//Sourabh Suneja
+//23070123129
+//entc b2
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int choice;
+    cout << "1. Monday" << endl
+         << "2. Tuesday" << endl
+         << "3. Wednesday" << endl
+         << "4. Thursday" << endl
+         << "5. Friday" << endl
+         << "6. Saturday" << endl
+         << "7. Sunday" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    
+    switch(choice) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Wrong Input" << endl;
+            break;
+    }
+    
+    return 0;
+}
---
+++ exp5D.cpp
@@ -0,0 +1,39 @@
+//Sourabh Suneja
+//23070123129
+//entc b2
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    char oper;
+    float num1, num2;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> oper;
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2;
+
+    switch (oper) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            else
+                cout << "Error! Division by zero." << endl;
+            break;
+        default:
+            cout << "Error! The operator is not correct" << endl;
+            break;
+    }
+
+    return 0;
+}
