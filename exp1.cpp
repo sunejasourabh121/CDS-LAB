e5c9dbf42fd2d7d5e0dee630f31716ac938e719d
Sourabh Suneja <64066437+sunejasourabh121@users.noreply.github.com>
2024-08-09 10:11:17 +0530

    Add files via upload

---
+++ exp1.cpp
@@ -0,0 +1,10 @@
+//Sourabh Suneja
+//23070123129
+//entc b2
+//experiment 1
+#include<iostream>
+int main()
+{
+    std::cout<<"hello world";
+    return 0;
+}
---
+++ exp1B.cpp
@@ -0,0 +1,17 @@
+///Sourabh Suneja
+//23070123129
+//entc b2
+//experiment 1
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,num3,sum=0;
+    cout<<"enter first number-";
+    cin>>num1;
+    cout<<"enter second number";
+    cin>>num2;
+    sum=num1+num2;
+    cout<<"sum of given numbers="<<sum;
+    return 0;
+}
