d405618c7efc40c7a789e7257d8e0f2461072755
Sourabh Suneja <64066437+sunejasourabh121@users.noreply.github.com>
2024-10-23 09:10:59 +0530

    Add files via upload

---
+++ 20B.cpp
@@ -0,0 +1,52 @@
+// NAME - Sourabh Suneja
+// PRN - 23070123129
+// EXPERIMENT - 20(B) 
+
+#include<iostream>
+using namespace std;
+
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int *a, int elements) {
+    int n = 0;
+    int *b;
+
+    while (n < elements) {
+        b = a + 1;
+        for (int i = 0; i < (elements - 1) - n; i++) {
+            if (*a > *b) {
+                swap(a, b);
+            }
+            b++;
+        }
+        n++;
+        a++;
+    }
+}
+
+int main() {
+    int no_el;
+    cout << "How many elements in your array?" << endl;
+    cin >> no_el;
+
+    int arr[no_el];
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < no_el; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, no_el);
+
+    cout << "Sorted array: " << endl;
+    for (int i = 0; i < no_el; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+} 
---
+++ 20C.cpp
@@ -0,0 +1,42 @@
+// NAME - Sourabh Suneja
+// PRN - 23070123129
+// EXPERIMENT - 20(C)
+
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    cout << "How many elements in your array?" << endl;
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
